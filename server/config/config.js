6f480b5b45c6359d966c3b3a19b4bb946efa657f
Cardenas Díaz <cpcardenas@indracompany.com>
2019-09-11 15:17:32 -0300

    add schema

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 //Puerto
 process.env.PORT = process.env.PORT||3000;
+
+//Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//BD
+let urlDB;
+/* if(process.env.NODE_ENV ==='dev'){
+    urlDB='mongodb://localhost:27017/cafe';
+}else{ */
+    urlDB='mongodb+srv://brujo:105236@pruebas-urzmd.mongodb.net/test?retryWrites=true&w=majority';
+//}
+
+process.env.URLDB = urlDB
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config.js');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 
 const bodyParser = require('body-parser');
  
@@ -9,29 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
  
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario');
-})
-
-app.post('/usuario', function (req, res) {
+app.use (require('./routes/usuario'));
   
-  let body =req.body;
-    res.json({
-      persona:body
-    });
-  })
+  mongoose.connect(process.env.URLDB, {useNewUrlParser: true});
 
-app.put('/usuario/:id', function (req, res) {
-   let id= req.params.id;   
-    res.json({
-      id
-    });
-  });
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario');
-  })
- 
-app.listen(process.env.PORT,()=>{
+ app.listen(process.env.PORT,()=>{
     console.log('Escuchando puerto ',process.env.PORT);
 })
